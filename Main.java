4c659efe206174a2501fd614ce931f17067f9ea2
NhatNguyen42 <nhatmn114@gmail.com>
2022-07-04 23:28:49 -0400

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Player1 player = new Player1("Nhat","sword",100);
+       System.out.println(player.getName());
+       System.out.println(player.getHealth());
+       System.out.println(player.getWeapon());
+
+
+    }
+
+}
